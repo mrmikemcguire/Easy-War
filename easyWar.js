aa4675b26fc791911874efd22340f27ba275806e
Michael McGuire <michaelmcguire@Michaels-MacBook-Pro.local>
2018-10-23 19:58:46 -0600

    first commit

---
+++ easyWar.js
@@ -0,0 +1,339 @@
+var deck = [
+    {
+	rank: "ace",
+	suit: "hearts",
+    value: 14,
+    face: "images/classic-cards/AH.png"
+    },
+    {
+    rank: "ace",
+	suit: "diamonds",
+    value: 14,
+    face: "images/classic-cards/AD.png"
+    },
+    {
+    rank: "ace",
+	suit: "spades",
+    value: 14,
+    face: "images/classic-cards/AS.png"
+    },
+    {
+    rank: "ace",
+	suit: "clubs",
+    value: 14,
+    face: "images/classic-cards/AC.png"
+    },
+    {
+    rank: "king",
+	suit: "hearts",
+    value: 13,
+    face: "images/classic-cards/KH.png"
+    },
+    {
+    rank: "king",
+	suit: "diamonds",
+    value: 13,
+    face: "images/classic-cards/KD.png"
+    },
+    {
+    rank: "king",
+	suit: "spades",
+    value: 13,
+    face: "images/classic-cards/KS.png"
+    },
+    {
+    rank: "king",
+	suit: "clubs",
+    value: 13,
+    face: "images/classic-cards/KC.png"
+    },
+    {
+    rank: "queen",
+	suit: "hearts",
+    value: 12,
+    face: "images/classic-cards/QH.png"
+    },
+    {
+    rank: "queen",
+	suit: "diamonds",
+    value: 12,
+    face: "images/classic-cards/QD.png"
+    },
+    {
+    rank: "queen",
+	suit: "spades",
+    value: 12,
+    face: "images/classic-cards/QS.png"
+    },
+    {
+    rank: "queen",
+	suit: "clubs",
+    value: 12,
+    face: "images/classic-cards/QC.png"
+    },
+    {
+    rank: "jack",
+	suit: "hearts",
+    value: 11,
+    face: "images/classic-cards/JH.png"
+    },
+    {
+    rank: "jack",
+	suit: "diamonds",
+    value: 11,
+    face: "images/classic-cards/JD.png"
+    },
+    {
+    rank: "jack",
+	suit: "spades",
+    value: 11,
+    face: "images/classic-cards/JS.png"
+    },
+    {
+    rank: "jack",
+	suit: "clubs",
+    value: 11,
+    face: "images/classic-cards/JC.png"
+    },
+    {
+    rank: "ten",
+	suit: "hearts",
+    value: 10,
+    face: "images/classic-cards/10H.png"
+    },
+    {
+    rank: "ten",
+	suit: "diamonds",
+    value: 10,
+    face: "images/classic-cards/10D.png"
+    },
+    {
+    rank: "ten",
+	suit: "spades",
+    value: 10,
+    face: "images/classic-cards/10S.png"
+    },
+    {
+    rank: "ten",
+	suit: "clubs",
+    value: 10,
+    face: "images/classic-cards/10C.png"
+    },
+    {
+    rank: "nine",
+	suit: "hearts",
+    value: 9,
+    face: "images/classic-cards/9H.png"
+    },
+    {
+    rank: "nine",
+	suit: "diamonds",
+    value: 9,
+    face: "images/classic-cards/9D.png"
+    },
+    {
+    rank: "nine",
+	suit: "spades",
+    value: 9,
+    face: "images/classic-cards/9S.png"
+    },
+    {
+    rank: "nine",
+	suit: "clubs",
+    value: 9,
+    face: "images/classic-cards/9C.png"
+    },
+    {
+    rank: "eight",
+	suit: "hearts",
+    value: 8,
+    face: "images/classic-cards/8H.png"
+    },
+    {
+    rank: "eight",
+	suit: "diamonds",
+    value: 8,
+    face: "images/classic-cards/8D.png"
+    },
+    {
+    rank: "eight",
+	suit: "spades",
+    value: 8,
+    face: "images/classic-cards/8S.png"
+    },
+    {
+    rank: "eight",
+	suit: "clubs",
+    value: 8,
+    face: "images/classic-cards/8C.png"
+    },
+    {
+    rank: "seven",
+	suit: "hearts",
+    value: 7,
+    face: "images/classic-cards/7H.png"
+    },
+    {
+    rank: "seven",
+	suit: "diamonds",
+    value: 7,
+    face: "images/classic-cards/7D.png"
+    },
+    {
+    rank: "seven",
+	suit: "spades",
+    value: 7,
+    face: "images/classic-cards/7S.png"
+    },
+    {
+    rank: "seven",
+	suit: "clubs",
+    value: 7,
+    face: "images/classic-cards/7C.png"
+    },
+    {
+    rank: "six",
+	suit: "hearts",
+    value: 6,
+    face: "images/classic-cards/6H.png"
+    },
+    {
+    rank: "six",
+	suit: "diamonds",
+    value: 6,
+    face: "images/classic-cards/6D.png"
+    },
+    {
+    rank: "six",
+	suit: "spades",
+    value: 6,
+    face: "images/classic-cards/6S.png"
+    },
+    {
+    rank: "six",
+	suit: "clubs",
+    value: 6,
+    face: "images/classic-cards/6C.png"
+    },
+    {
+    rank: "five",
+	suit: "hearts",
+    value: 5,
+    face: "images/classic-cards/5H.png"
+    },
+    {
+    rank: "five",
+	suit: "diamonds",
+    value: 5,
+    face: "images/classic-cards/5D.png"
+    },
+    {
+    rank: "five",
+	suit: "spades",
+    value: 5,
+    face: "images/classic-cards/5S.png"
+    },
+    {
+    rank: "five",
+	suit: "clubs",
+    value: 5,
+    face: "images/classic-cards/5C.png"
+    },
+    {
+    rank: "four",
+	suit: "hearts",
+    value: 4,
+    face: "images/classic-cards/4H.png"
+    },
+    {
+    rank: "four",
+	suit: "diamonds",
+    value: 4,
+    face: "images/classic-cards/4D.png"
+    },
+    {
+    rank: "four",
+	suit: "spades",
+    value: 4,
+    face: "images/classic-cards/4S.png"
+    },
+    {
+    rank: "four",
+	suit: "clubs",
+    value: 4,
+    face: "images/classic-cards/4C.png"
+    },
+    {
+    rank: "three",
+	suit: "hearts",
+    value: 3,
+    face: "images/classic-cards/3H.png"
+    },
+    {
+    rank: "three",
+	suit: "diamonds",
+    value: 3,
+    face: "images/classic-cards/3D.png"
+    },
+    {
+    rank: "three",
+	suit: "spades",
+    value: 3,
+    face: "images/classic-cards/3S.png"
+    },
+    {
+    rank: "three",
+	suit: "clubs",
+    value: 3,
+    face: "images/classic-cards/3C.png"
+    },
+    {
+    rank: "two",
+	suit: "hearts",
+    value: 2,
+    face: "images/classic-cards/2H.png"
+    },
+    {
+    rank: "two",
+	suit: "diamonds",
+    value: 2,
+    face: "images/classic-cards/2D.png"
+    },
+    {
+    rank: "two",
+	suit: "spades",
+    value: 2,
+    face: "images/classic-cards/2S.png"
+    },
+    {
+    rank: "two",
+	suit: "clubs",
+    value: 2,
+    face: "images/classic-cards/2C.png"
+    } ];
+
+var randomNumber1 = Math.floor(Math.random() * 52);
+var randomNumber2 = Math.floor(Math.random() * 52);
+
+document.write("You drew a " + 
+               deck[randomNumber1].rank + " of " +
+               deck[randomNumber1].suit);
+var yourScore = deck[randomNumber1].value;
+
+document.write("<br>The computer drew a " + 
+               deck[randomNumber2].rank + " of " +
+               deck[randomNumber2].suit);
+var computerScore = deck[randomNumber2].value;
+
+if (yourScore > computerScore)
+    {
+    document.write("<br> You win!");
+    }
+else if (yourScore < computerScore)
+    {
+    document.write("<br> You lose!");
+    }
+else
+    {
+    document.write("<br> It is a tie!");
+    }
